bef5e1fa1fd03b4a353a0c60e7f1d161b4ec4d2f
Adam Kabak <adamkabak@yahoo.com>
2022-09-21 09:44:08 -0400

    commit tree 7beff0bb0ffbf0fcc7c430f17aad58c2bffbe1bf

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+const replaceText = () => {
+    const pText = document.querySelector('#text');
+    pText.textContent = 'This is really cool!';
+}
+document.addEventListener('DOMContentLoaded', replaceText)
